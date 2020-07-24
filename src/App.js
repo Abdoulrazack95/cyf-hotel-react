e30ca70c69e844fd4948c15d0622e9c1e3e5e00e
Abdoulrazack95 <abdoulrazackkahin@gmail.com>
2020-07-24 14:22:41 +0100

    Object destructuring instead of props

--- src/App.js
+++ src/App.js
@@ -4,21 +4,54 @@ import Bookings from "./Bookings";
 import Restaurant from "./Restaurant";
 import "./App.css";
 import Heading from "./Heading";
-import TouristInfoCards from './TouristInfoCards';
-import Footer from './Footer';
+import TouristInfoCards from "./TouristInfoCards";
+import Footer from "./Footer";
 class App extends Component {
-  render() { 
+  render() {
     return (
       <div className="App">
         <Heading />
-        <div className = 'cardContainer'>
-          <TouristInfoCards source="https://dimg.visitscotland.com/wsimgs/visitscotland_26057864194_1356855539.jpg" link ="https://peoplemakeglasgow.com" paragraph="Willing to visite monumental buildings? in Glasgow you will find every types of fun " cityName = 'Glassgow'/>
-          <TouristInfoCards source="https://upload.wikimedia.org/wikipedia/commons/6/69/Piccadilly-Gardens-Manchester-UK-1.JPG" paragraph="Willing to visite monumental buildings? in Glasgow you will find every types of fun" cityName = 'Manchester'/>
-          <TouristInfoCards source="https://www.cbronline.com/wp-content/uploads/2017/02/UK.jpg" link ="https://visitlondon.com" paragraph="Willing to visite monumental buildings? in Glasgow you will find every types of fun " cityName = 'london'/>
+        <div className="cardContainer">
+          <TouristInfoCards
+            cardDetails={{
+              source:
+                "https://dimg.visitscotland.com/wsimgs/visitscotland_26057864194_1356855539.jpg",
+              link: "https://peoplemakeglasgow.com",
+              paragraph:
+                "Willing to visite monumental buildings? in Glasgow you will find every types of fun ",
+              cityName: "Glassgow",
+            }}
+          />
+          <TouristInfoCards
+            cardDetails={{
+              source:
+                "https://upload.wikimedia.org/wikipedia/commons/6/69/Piccadilly-Gardens-Manchester-UK-1.JPG",
+              paragraph:
+                "Willing to visite monumental buildings? in Glasgow you will find every types of fun",
+              cityName: "Manchester",
+            }}
+          />
+          <TouristInfoCards
+            cardDetails={{
+              source:
+                "https://www.cbronline.com/wp-content/uploads/2017/02/UK.jpg",
+              link: "https://visitlondon.com",
+              paragraph:
+                "Willing to visite monumental buildings? in Glasgow you will find every types of fun ",
+              cityName: "london",
+            }}
+          />
         </div>
         <Bookings />
         <Restaurant />
-        <Footer array1={["123 Fake Street, London, E1 4UD", "hello@fakehotel.com", "0123 456789"]}me = 'footer'/>
+        <Footer
+          array1={[
+            "123 Fake Street, London, E1 4UD",
+            "hello@fakehotel.com",
+            "0123 456789",
+          ]}
+          me="footer"
+        />
       </div>
     );
   }
--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const TouristInfoCards = (props) =>{
-    return  (
-        <div className="card" style={{width: "18rem"}}>
-            <img src={props.source} className="card-img-top" alt="..."/>
-            <div class="card-body">
-                <h5 className="card-title">{props.cityName}</h5>
-                <p className="card-text">{props.paragraph}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    )
-}
+const TouristInfoCards = ({ cardDetails }) => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img src={cardDetails.source} className="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 className="card-title">{cardDetails.cityName}</h5>
+        <p className="card-text">{cardDetails.paragraph}</p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default TouristInfoCards;

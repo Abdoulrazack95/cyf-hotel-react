0f7c1287e748aa93bba830124c540f56e7b439ca
Abdoulrazack95 <abdoulrazackkahin@gmail.com>
2020-07-24 13:58:56 +0100

    use state and use effet hooks in clock

--- src/Clock.js
+++ src/Clock.js
@@ -1,26 +1,37 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 
-class Clock extends Component {
-  constructor(props) {
-    super(props);
+function Clock() {
+  const [date, setDate] = useState(new Date());
 
-    this.state = { date: new Date() };
-  }
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+  }, []);
 
-  componentDidMount() {
-    // There is a bug in this component, where this.timer is not cleared if the
-    // component is unmounted. It will continue to try to render if even the
-    // component is no longer in the DOM.
-    this.timer = setInterval(this.tick, 1000);
-  }
-
-  tick = () => {
-    this.setState({ date: new Date() });
-  };
-
-  render() {
-    return <div className="clock">{this.state.date.toLocaleTimeString()}</div>;
-  }
+  return <div className="clock">{date.toLocaleTimeString()}</div>;
 }
+// class Clock extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = { date: new Date() };
+//   }
+
+//   componentDidMount() {
+//     // There is a bug in this component, where this.timer is not cleared if the
+//     // component is unmounted. It will continue to try to render if even the
+//     // component is no longer in the DOM.
+//     this.timer = setInterval(this.tick, 1000);
+//   }
+
+//   tick = () => {
+//     this.setState({ date: new Date() });
+//   };
+
+//   render() {
+//     return <div className="clock">{this.state.date.toLocaleTimeString()}</div>;
+//   }
+// }
 
 export default Clock;

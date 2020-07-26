2f2f7ce20355bf65019b8e00ff03c057cf418ddd
Abdoulrazack95 <abdoulrazackkahin@gmail.com>
2020-07-26 15:31:37 +0100

    useState useEffet hooks

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,46 +1,79 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResult from "./SearchResult.js";
-import FakeBookings from './data/fakeBookings.json';
+import FakeBookings from "./data/fakeBookings.json";
 
-export default class Bookings extends Component {
-  constructor(props){
-    super(props);
-    this.state= {
-      fakeBookings: null
-    }
-  }
+function Bookings() {
+  const [fakeBookings, setFakeBooking] = useState([]);
 
-  componentDidMount() {
-    fetch(`https://cyf-react.glitch.me/delayed`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-        fakeBookings: data
-        })
-      })
-  }
-
-  search = searchVal => {
-    let infoarray = this.state.fakeBookings.filter(x => x.firstName.toLowerCase() === searchVal.toLowerCase() || x.surname.toLowerCase() === searchVal.toLowerCase())
-    if(infoarray.length === 0){
-      this.setState({
-        fakeBookings: this.state.fakeBookings
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me/delayed")
+      .then((res) => res.json())
+      .then((data) => {
+        setFakeBooking(data);
       });
-    }else{
-      this.setState({
-        fakeBookings: infoarray
-      });
-    }
-  }
-  render() {
-    return (
-      <div className="App-content">
-        <div className="container">
-          <Search search={this.search} />
-          {(this.state.fakeBookings)? <SearchResult results={this.state.fakeBookings} /> : <h1><i class="fas fa-spinner fa-3x"></i></h1>}
-        </div>
+  }, []);
+
+  return (
+    <div className="App-content">
+      <div className="container">
+        <Search />
+        <SearchResult results={fakeBookings} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Bookings;
+
+// export default class Bookings extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       fakeBookings: null,
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch(`https://cyf-react.glitch.me/delayed`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         this.setState({
+//           fakeBookings: data,
+//         });
+//       });
+//   }
+
+//   search = (searchVal) => {
+//     let infoarray = this.state.fakeBookings.filter(
+//       (x) =>
+//         x.firstName.toLowerCase() === searchVal.toLowerCase() ||
+//         x.surname.toLowerCase() === searchVal.toLowerCase()
+//     );
+//     if (infoarray.length === 0) {
+//       this.setState({
+//         fakeBookings: this.state.fakeBookings,
+//       });
+//     } else {
+//       this.setState({
+//         fakeBookings: infoarray,
+//       });
+//     }
+//   };
+//   render() {
+//     return (
+//       <div className="App-content">
+//         <div className="container">
+//           <Search search={this.search} />
+//           {this.state.fakeBookings ? (
+//             <SearchResult results={this.state.fakeBookings} />
+//           ) : (
+//             <h1>
+//               <i class="fas fa-spinner fa-3x">looding...</i>
+//             </h1>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
